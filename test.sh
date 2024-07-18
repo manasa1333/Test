20b63bb6279db5a7d7cc62ac2d26501fd1a6d547
manasa1333 <manasas1333@gmail.com>
2024-07-18 15:24:49 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,19 +2,19 @@
  
 echo "Running iteration 10 Threads"
 
-cd /home/cloud-user/vali/Internal_API_Regression/apache-jmeter-5.6.3/bin
+cd /opt/Internal/apache-jmeter-5.6.3/bin
 sudo sh jmeter.sh -n -t internalAPIs_10threads.jmx -l regression_10_threads.jtl
 
-cd /home/cloud-user/vali/Internal_API_Regression/lics/apache-jmeter-5.6.3/bin
+cd /opt/Internal/lics/apache-jmeter-5.6.3/bin
 sudo sh jmeter.sh -n -t internalAPIs_lics_metering_10threads.jmx -l lics_regression_10_threads.jtl
 sleep 300
  
 echo "Running iteration Production Throughput"
 
-cd /home/cloud-user/vali/Internal_API_Regression/apache-jmeter-5.6.3/bin
+cd /opt/Internal/apache-jmeter-5.6.3/bin
 sudo sh jmeter.sh -n -t internalAPIs_prod_throughput_all.jmx -l regression_prod_throughput.jtl
 
-cd /home/cloud-user/vali/Internal_API_Regression/lics/apache-jmeter-5.6.3/bin
+cd /opt/Internal/lics/apache-jmeter-5.6.3/bin
 sudo sh jmeter.sh -n -t internalAPIs_lics_throughput.jmx -l lics_regression_prod_throughput.jtl
  
 echo "All iterations completed."
